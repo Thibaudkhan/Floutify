9392b3deaf3a274bc5be20ed4f59cb9fe5bf679c
Sarah Visage <sarahvisage@MacBook-Pro.local>
2021-01-07 10:33:41 +0100

    Initial Project

---
+++ Floutify/Floutify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Floutify
+//
+//  Created by Sarah Visage on 07/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
