81b6387f65136bb6ee6259c6433ee842dc847d87
mathieu <mathieusoufflard@gmail.com>
2021-01-07 18:12:54 +0100

    switch to connection page to the next one

--- Floutify/Floutify/Controler/LoginViewController.swift
+++ Floutify/Floutify/Controler/LoginViewController.swift
@@ -2,20 +2,34 @@
 //  LoginViewController.swift
 //  Floutify
 //
-//  Created by Sarah Visage on 07/01/2021.
+//  Created by user185203 on 1/7/21.
 //
 
 import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtLogin: UITextField!
+    @IBOutlet weak var txtPwd: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func btnConnect(_ sender: Any) {
+        if(txtLogin.text == "" && txtPwd.text == "")
+        {
+            print("connected")
+            
+            let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+            let nextViewController = storyBoard.instantiateViewController(withIdentifier: "artist") as! ArtistViewController
+            self.present(nextViewController, animated:true, completion:nil)
+        }
 
+    }
+    
     /*
     // MARK: - Navigation
 
