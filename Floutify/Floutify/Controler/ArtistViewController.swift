f128483280e4fa6a693caa6144b1e29a3120717f
Sarah Visage <sarahvisage@MacBook-Pro.local>
2021-01-08 06:16:11 +0100

    Beginning integration

--- Floutify/Floutify/Controler/ArtistViewController.swift
+++ Floutify/Floutify/Controler/ArtistViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 var album: [String] = ["Cobble","Terre"]
 var myIndex = 0
 
+
 class ArtistViewController: UIViewController {
 
     var index = 0
@@ -18,20 +19,31 @@ class ArtistViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
         var images: [ImageCell] = []
+        var artists: [Artist] = []
+        var artistsList: [Artist] {
+          get { return artists }
+          set(value) { artists = value}
+           
+        }
         
         override func viewDidLoad() {
             super.viewDidLoad()
             
-            images = createArray()
+            getJSON()
             tableView.delegate = self
             tableView.dataSource = self
-            
-
-            // Do any additional setup after loading the view.
         }
+    
         
-        func createArray() -> [ImageCell]{
+        func loadArray(artists: [Artist]) {
+            images = createArray(artists: artists)
+        }
+    
+    
+        func createArray(artists: [Artist]) -> [ImageCell]{
             
+            
+            print("OSEKUUUUUUUUUUR \(artists)")
             var allImages: [ImageCell] = []
             let txt = "CobblestoneNew.png"
                 
@@ -44,7 +56,47 @@ class ArtistViewController: UIViewController {
             return allImages
         }
 
-   
+        // Integration of the JSON datas //
+        func getJSON() {
+            
+            print("GetJSON !!!!!!!")
+            //URL of the JSON
+            let urlString = "https://gist.githubusercontent.com/jasonbaldridge/2668632/raw/e56320c485a33c339791a25cc107bf70e7f1d763/music.json"
+
+            // Transform the Url String to an URL Object
+            let url = URL(string: urlString)
+
+            guard url != nil else {
+                return
+            }
+
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: url!) { [self] (data, response, error) in
+                
+                // Check for errors
+                if error == nil && data != nil {
+        
+                    // Parse JSON
+                    let decoder = JSONDecoder()
+                    
+                    do {
+                        
+                        let json_decoded = try decoder.decode([Artist].self, from: data!)
+                        //artistsList.append(contentsOf: json_decoded)
+                        artistsList = json_decoded
+                        loadArray(artists: json_decoded)
+
+                        print(artistsList)
+                    }
+                    catch {
+                        print("Error JSON Parsing : \(error)")
+                    }
+                }
+            }
+            
+            // Make the API Call
+            dataTask.resume()
+        }
 }
 
 
